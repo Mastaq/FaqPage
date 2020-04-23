c775177f93063f9395e19422ed90a306d0e1f960
Sergei Sergeev <sergeev.srg@gmail.com>
2020-04-24 00:21:41 +0300

    Fixed link search issue.

--- FaqPage/Scripts/App/Filters/highlight.js
+++ FaqPage/Scripts/App/Filters/highlight.js
@@ -3,8 +3,8 @@
 	angular.module("FaqApp.filters")
 		.filter("faqhighlight", ["$sce", function ($sce) {
 			return function(text, phrase) {
-				if (phrase) {
-					text = text.replace(new RegExp(phrase, "gi"), "<span class='highlighted'>$&</span>");
+                if (phrase) {
+                    text = text.replace(new RegExp("(?<!<\/?[^>]*|&[^;]*)(" + phrase + ")", "gi"), "<span class='highlighted'>$&</span>");
 				}
 				return $sce.trustAsHtml(text);
 			}
--- FaqPage/gruntfile.js
+++ FaqPage/gruntfile.js
@@ -2,6 +2,8 @@
 // This file in the main entry point for defining grunt tasks and using grunt plugins.
 // Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 
+const sass = require('node-sass');
+
 module.exports = function (grunt) {
 
 	var scriptsPath = "Scripts";
@@ -105,7 +107,7 @@ module.exports = function (grunt) {
 
 		sass: {
 			options: {
-
+				implementation: sass
 			},
 			main: {
 				files: [{
